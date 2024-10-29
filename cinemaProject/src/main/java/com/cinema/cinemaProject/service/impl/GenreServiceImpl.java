bae0547699f01c281af9a40e8943affdf1942cab
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2024-10-29 20:07:56 +0100

    added genre deletion with associated movies

--- cinemaProject/src/main/java/com/cinema/cinemaProject/service/impl/GenreServiceImpl.java
+++ cinemaProject/src/main/java/com/cinema/cinemaProject/service/impl/GenreServiceImpl.java
@@ -3,7 +3,9 @@ package com.cinema.cinemaProject.service.impl;
 import com.cinema.cinemaProject.exception.genres.GenresListEmpty;
 import com.cinema.cinemaProject.exception.genres.GenreNotFoundException;
 import com.cinema.cinemaProject.model.Genre;
+import com.cinema.cinemaProject.model.Movie;
 import com.cinema.cinemaProject.repository.GenreRepository;
+import com.cinema.cinemaProject.repository.MovieRepository;
 import com.cinema.cinemaProject.service.GenreService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -15,6 +17,8 @@ public class GenreServiceImpl implements GenreService {
 
 
     private final GenreRepository genreRepository;
+
+    private final MovieRepository movieRepository;
 
     @Override
     public Genre findOne(Long genreId) {
@@ -48,6 +52,12 @@ public class GenreServiceImpl implements GenreService {
     public Genre delete(Long genreId) {
         Genre genre = genreRepository.findOneById(genreId);
         if(genre!=null){
+            for (Movie movie : movieRepository.findAll()) {
+                if (movie.getGenres().contains(genre)) {
+                    movie.getGenres().remove(genre);
+                    movieRepository.save(movie);  // Save updated movie to persist changes
+                }
+            }
             genreRepository.delete(genre);
             return genre;
         }
