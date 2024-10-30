bc073c5a799b0d892504e220eca15486870e56f6
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2024-10-30 23:59:31 +0100

    Update Loader.tsx

--- cinemaProject/frontend/src/components/shared/loader/Loader.tsx
+++ cinemaProject/frontend/src/components/shared/loader/Loader.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { ClipLoader } from 'react-spinners';
 
 interface LoaderProps {
   loading: boolean;
 }
 
 const loader = ({ loading }: LoaderProps) => {
-  return <div></div>;
+  <div className="loader">
+    <ClipLoader loading={loading} size={50} />
+  </div>;
 };
 
 export default loader;
