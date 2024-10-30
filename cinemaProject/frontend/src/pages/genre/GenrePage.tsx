5ba7f5c904cb62a680b1bb506dc6fbab49f8aadc
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2024-10-31 00:47:25 +0100

    Update GenrePage.tsx

--- cinemaProject/frontend/src/pages/genre/GenrePage.tsx
+++ cinemaProject/frontend/src/pages/genre/GenrePage.tsx
@@ -1,6 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Genre } from '../../types/Genre';
+import { useNavigate } from 'react-router-dom';
+import genreServiceAxios from '../../components/api/genre.service.axios';
+import { error } from 'console';
 
 const GenrePage = () => {
+  const [genres, setGenres] = useState<Genre[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const navigate = useNavigate();
+
+  const getGenres = async () => {
+    try {
+      const res = await genreServiceAxios.getGenres();
+      console.log(res.data);
+      setGenres(res.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getGenres();
+  }, []);
   return <div></div>;
 };
 
