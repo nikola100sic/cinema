ff2f3f8d44bf650af05893a5d13962cf8119a781
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2024-10-31 00:46:56 +0100

    Update genre.service.axios.ts

--- cinemaProject/frontend/src/components/api/genre.service.axios.ts
+++ cinemaProject/frontend/src/components/api/genre.service.axios.ts
@@ -1,18 +1,25 @@
 import { CinemaAxios } from './axios';
 import { Genre } from '../../types/Genre';
 
-export const getGenres = () => {
+const getGenres = () => {
   return CinemaAxios.get('/genres');
 };
 
-export const getGenre = (id: string) => {
+const deleteGenre = (id: string) => {
   return CinemaAxios.get(`/genres/${id}`);
 };
 
-export const addGenre = (genre: Genre) => {
+const addGenre = (genre: Genre) => {
   return CinemaAxios.post('/genres/', genre);
 };
 
-export const updateGenre = (updatedGenre: Genre) => {
+const updateGenre = (updatedGenre: Genre) => {
   return CinemaAxios.put(`/genres`, updateGenre);
+};
+
+export default {
+  getGenres,
+  addGenre,
+  updateGenre,
+  deleteGenre,
 };
