49f74a44b7f2fe0e5e7cf475e198bdb2d8c51856
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2024-10-31 00:34:12 +0100

    Add react-toastify

--- cinemaProject/frontend/src/App.tsx
+++ cinemaProject/frontend/src/App.tsx
@@ -2,12 +2,29 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import './App.css';
 import Layout from './components/shared/layout/Layout';
 import Button from './components/shared/button/Button';
+import { ToastContainer } from 'react-toastify';
 
 function App() {
   return (
-    <Router>
-      <Layout></Layout>
-    </Router>
+    <>
+      <Router>
+        <Layout></Layout>
+      </Router>
+
+      <ToastContainer
+        position="top-center"
+        autoClose={2000}
+        hideProgressBar
+        rtl={false}
+        pauseOnFocusLoss
+        theme="light"
+        style={{
+          position: '-moz-initial',
+          top: '5%',
+          zIndex: 9999,
+        }}
+      />
+    </>
   );
 }
 
