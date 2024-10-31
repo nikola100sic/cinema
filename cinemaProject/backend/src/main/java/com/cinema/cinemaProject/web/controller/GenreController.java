d826101d83c95fc1b19d0ead15b41601722e68c3
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2024-10-31 01:01:26 +0100

    Update GenreController.java

--- cinemaProject/backend/src/main/java/com/cinema/cinemaProject/web/controller/GenreController.java
+++ cinemaProject/backend/src/main/java/com/cinema/cinemaProject/web/controller/GenreController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping(value = "/api/genres", produces = MediaType.APPLICATION_JSON_VALUE)
 public class GenreController {
