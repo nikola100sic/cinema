ef4c46c54de6d0a4cdec5995fe67b8ecbafb431a
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2024-10-31 16:28:57 +0100

    Update SecurityConfig

--- cinemaProject/backend/src/main/java/com/cinema/cinemaProject/config/SecurityConfig.java
+++ cinemaProject/backend/src/main/java/com/cinema/cinemaProject/config/SecurityConfig.java
@@ -15,7 +15,7 @@ public class SecurityConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
                         .allowedOrigins("http://localhost:3000")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") 
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true)
                         .maxAge(3600);
