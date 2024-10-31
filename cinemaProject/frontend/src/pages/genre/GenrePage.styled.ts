749f275dc6d2b5e1e00e9beddad17f65c36f1231
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2024-11-01 00:17:30 +0100

    Update GenrePage.styled.ts

--- cinemaProject/frontend/src/pages/genre/GenrePage.styled.ts
+++ cinemaProject/frontend/src/pages/genre/GenrePage.styled.ts
@@ -1,8 +1,22 @@
 import styled from 'styled-components';
+import { StyledButton } from '../../components/shared/button/Button.styled';
 
 export const CardContainer = styled.div`
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
   gap: 30px;
+`;
+
+export const AddButtonContainer = styled.div`
+  display: flex;
+  margin-top: 30px;
+  justify-content: center;
+`;
+
+export const AddButton = styled(StyledButton)`
+  background-color: #00bcf7;
+  border: 1px solid #4a393952;
+  border-radius: 10px;
+  width: 129px;
 `;
