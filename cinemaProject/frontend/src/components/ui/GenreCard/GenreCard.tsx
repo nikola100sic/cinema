703aa6133a13f0f79365450462acc6f192d96f2a
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2024-11-01 00:16:39 +0100

    Update GenreCard.tsx

--- cinemaProject/frontend/src/components/ui/GenreCard/GenreCard.tsx
+++ cinemaProject/frontend/src/components/ui/GenreCard/GenreCard.tsx
@@ -4,16 +4,19 @@ import { ButtonContainer, Card, GenreInfo } from './GenreCard.styled';
 
 interface GenreCardProps {
   name: string;
+  id: number;
+  onDelete: (id: number) => void; // Dodaj onDelete prop
 }
 
-const GenreCard = ({ name }: GenreCardProps) => {
+const GenreCard = ({ id, name, onDelete }: GenreCardProps) => {
   return (
     <Card>
       <GenreInfo>
         <h3>{name}</h3>
       </GenreInfo>
       <ButtonContainer>
-        <Button text="Delete"></Button>
+        <Button text="Delete" onClick={() => onDelete(id)} />{' '}
+        {/* Poziv na onDelete */}
         <Button text="Edit" type="submit"></Button>
       </ButtonContainer>
     </Card>
