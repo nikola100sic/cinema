152e6c6327532fe5b2927364b0a73d758b7d6b5d
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2024-11-01 00:17:03 +0100

    Create GenreAddPage.tsx

---
+++ cinemaProject/frontend/src/pages/genre/GenreAddPage.tsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+const GenreAddPage = () => {
+  const [genre, setGenre] = useState({
+    name: '',
+  });
+
+  return <div></div>;
+};
+
+export default GenreAddPage;
