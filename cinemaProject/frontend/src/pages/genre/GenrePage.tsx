20823dbb681efc5895d38f8fe1a94a0a03491456
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2024-11-01 00:17:52 +0100

    Update GenrePage.tsx

--- cinemaProject/frontend/src/pages/genre/GenrePage.tsx
+++ cinemaProject/frontend/src/pages/genre/GenrePage.tsx
@@ -4,7 +4,13 @@ import { useNavigate } from 'react-router-dom';
 import genreServiceAxios from '../../components/api/genre.service.axios';
 import GenreCard from '../../components/ui/GenreCard/GenreCard';
 import Loader from '../../components/shared/loader/Loader';
-import { CardContainer } from './GenrePage.styled';
+import {
+  AddButton,
+  AddButtonContainer,
+  CardContainer,
+} from './GenrePage.styled';
+import { toast } from 'react-toastify';
+import Button from '../../components/shared/button/Button';
 
 const GenrePage = () => {
   const [genres, setGenres] = useState<Genre[]>([]);
@@ -25,6 +31,46 @@ const GenrePage = () => {
   useEffect(() => {
     getGenres();
   }, []);
+
+  const handleDelete = (id: number) => {
+    console.log(`Handle delete called with id: ${id}`); // Debug log
+    console.log('Attempting to show toast'); // Debug log
+    toast(
+      <div>
+        <div>Are you sure you want to delete this genre?</div>
+        <Button text="Yes" variant="filled" onClick={() => confirmDelete(id)} />
+        <Button text="No" variant="outlined" onClick={() => toast.dismiss()} />
+      </div>,
+      {
+        autoClose: false,
+        closeButton: false,
+      },
+    );
+  };
+
+  const confirmDelete = (id: number) => {
+    console.log(`Attempting to confirm delete for genre with id: ${id}`); // Debug log
+    toast.dismiss();
+
+    genreServiceAxios
+      .deleteGenre(id)
+      .then((res) => {
+        console.log(res);
+        toast.success('Successfully deleted');
+        setGenres((prevGenres) =>
+          prevGenres.filter((genre) => genre.id !== id),
+        );
+      })
+      .catch((error) => {
+        console.log(error);
+        toast.error('Error');
+      });
+  };
+
+  const goToAdd = () => {
+    navigate('/genres/add');
+  };
+
   return (
     <>
       {loading ? (
@@ -32,10 +78,18 @@ const GenrePage = () => {
       ) : (
         <CardContainer>
           {genres.map((genre) => (
-            <GenreCard key={genre.id} name={genre.name} />
+            <GenreCard
+              key={genre.id}
+              name={genre.name}
+              id={genre.id}
+              onDelete={handleDelete}
+            />
           ))}
         </CardContainer>
       )}
+      <AddButtonContainer>
+        <AddButton onClick={() => goToAdd()}>Add new genre</AddButton>
+      </AddButtonContainer>
     </>
   );
 };
