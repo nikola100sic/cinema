73e3b1fbdb88ef6e52d6e2711d23bcf6217dc406
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2024-10-31 16:25:17 +0100

    Create GenreCard.styled.ts

---
+++ cinemaProject/frontend/src/components/ui/GenreCard/GenreCar.styled.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const Card = styled.div`
+  display: flex;
+  background-color: #0077ff45;
+  flex-direction: column;
+  align-items: center;
+  flex: 1 1 calc(33.33% - 20px);
+`;
+
+export const GenreInfo = styled.div`
+  display: flex;
+`;
+
+export const ButtonContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 35px;
+`;
