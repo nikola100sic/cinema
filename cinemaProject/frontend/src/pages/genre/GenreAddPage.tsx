8540941d9b380c2814a5e8aa6920c5f140792875
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2024-11-02 00:35:44 +0100

    Update GenreAddPage.tsx

--- cinemaProject/frontend/src/pages/genre/GenreAddPage.tsx
+++ cinemaProject/frontend/src/pages/genre/GenreAddPage.tsx
@@ -1,11 +1,20 @@
 import React, { useState } from 'react';
+import Button from '../../components/shared/button/Button';
 
 const GenreAddPage = () => {
   const [genre, setGenre] = useState({
     name: '',
   });
 
-  return <div></div>;
+  return (
+    <div>
+      <form>
+        <label>Name</label>
+        <input type="text" placeholder="Enter genre name"></input>
+        <Button text="Add" type="submit" />
+      </form>
+    </div>
+  );
 };
 
 export default GenreAddPage;
