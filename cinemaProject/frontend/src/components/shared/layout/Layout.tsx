95ddd538d8facb4b727716ea67d980dd5facf7ef
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2024-11-02 23:28:32 +0100

    Edited Layout.tsx

--- cinemaProject/frontend/src/components/shared/layout/Layout.tsx
+++ cinemaProject/frontend/src/components/shared/layout/Layout.tsx
@@ -2,7 +2,6 @@ import React, { ReactNode } from 'react';
 import { Content, StyledPageContainer } from './Layout.styled';
 import Header from '../header/Header';
 import Footer from '../footer/Footer';
-import path from 'path';
 
 const NAVITEMS = [
   {
