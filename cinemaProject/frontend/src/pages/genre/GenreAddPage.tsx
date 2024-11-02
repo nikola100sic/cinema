d3b40278d2247d8752fd3d7547941f591a2b5f49
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2024-11-02 01:26:51 +0100

    Add genre addition functionality to component

--- cinemaProject/frontend/src/pages/genre/GenreAddPage.tsx
+++ cinemaProject/frontend/src/pages/genre/GenreAddPage.tsx
@@ -1,19 +1,63 @@
 import React, { useState } from 'react';
 import Button from '../../components/shared/button/Button';
+import {
+  AddPageStyled,
+  FormInput,
+  FormLabel,
+  FormTitle,
+} from '../../components/shared/forms/Forms.styled';
+import { toast } from 'react-toastify';
+import genreServiceAxios from '../../components/api/genre.service.axios';
+import { useNavigate } from 'react-router-dom';
 
 const GenreAddPage = () => {
   const [genre, setGenre] = useState({
+    id: -1,
     name: '',
   });
+  const navigate = useNavigate();
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setGenre((prevGenre) => ({
+      ...prevGenre,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (genre.name === '') {
+      toast.info('You must enter a name of genre');
+      return;
+    }
+    genreServiceAxios
+      .addGenre(genre)
+      .then((res) => {
+        toast.success('Genre added successfully!');
+        navigate('/genres');
+      })
+      .catch((error) => {
+        console.log(error);
+        toast.error('Error adding genre');
+      });
+  };
 
   return (
-    <div>
-      <form>
-        <label>Name</label>
-        <input type="text" placeholder="Enter genre name"></input>
+    <AddPageStyled>
+      <form onSubmit={handleSubmit}>
+        <FormTitle>Add genre</FormTitle>
+        <FormLabel>Name:</FormLabel>
+        <FormInput
+          type="text"
+          placeholder="Enter genre name"
+          onChange={handleChange}
+          name="name"
+          value={genre.name}
+        ></FormInput>
         <Button text="Add" type="submit" />
       </form>
-    </div>
+    </AddPageStyled>
   );
 };
 
