c2fa4deb465547b76fdd71b5c7ca90f269c8952a
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2024-11-03 13:32:44 +0100

    Updated MovieCard.styled.ts

--- cinemaProject/frontend/src/components/ui/MovieCard/MovieCard.styled.ts
+++ cinemaProject/frontend/src/components/ui/MovieCard/MovieCard.styled.ts
@@ -1,3 +1,22 @@
 import styled from 'styled-components';
 
-const MovieCard = styled.div``;
+export const MovieCardStyled = styled.div`
+  display: flex;
+  background-color: #4e96c73b;
+  padding: 13px;
+  border-radius: 18px;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const MovieInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+  align-items: center;
+`;
+
+export const Label = styled.span`
+  font-weight: bold;
+  font-size: 16px;
+`;
