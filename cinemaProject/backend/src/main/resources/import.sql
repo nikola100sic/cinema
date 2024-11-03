6c8c7da3edb0c14f58848584f3dea8fbf46bcb08
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2024-11-03 22:27:49 +0100

    Updated import.sql test data

--- cinemaProject/backend/src/main/resources/import.sql
+++ cinemaProject/backend/src/main/resources/import.sql
@@ -5,13 +5,20 @@ INSERT INTO genres (id, name) VALUES (4, 'Drama');
 INSERT INTO genres (id, name) VALUES (5, 'Science fiction');
 INSERT INTO genres (id, name) VALUES (6, 'Romance');
 
-INSERT INTO movies (name, duration, image) VALUES ('The Godfather', 175, 'hwww.movie1.com');
-INSERT INTO movies (name, duration, image) VALUES ('Meet the Fockers', 152, 'www.movie2.com');
-INSERT INTO movies (name, duration, image) VALUES ('Harry Poter', 154, 'www.movie3.com');
+INSERT INTO movies (name, duration, image) VALUES ('The Godfather', 140, 'image1');
+INSERT INTO movies (name, duration, image) VALUES ('Meet the Fockers', 162, 'image2');
+INSERT INTO movies (name, duration, image) VALUES ('Joker', 178, 'image3');
+INSERT INTO movies (name, duration, image) VALUES ('Titanic', 150, 'image4');
 
-INSERT INTO movie_genre (movie_id, genre_id) VALUES (1,1);
-INSERT INTO movie_genre (movie_id, genre_id) VALUES (1,2);
+
+INSERT INTO movie_genre (movie_id, genre_id) VALUES (1,3);
+INSERT INTO movie_genre (movie_id, genre_id) VALUES (1,4);
 INSERT INTO movie_genre (movie_id, genre_id) VALUES (2,1);
+INSERT INTO movie_genre (movie_id, genre_id) VALUES (2,6);
 INSERT INTO movie_genre (movie_id, genre_id) VALUES (3,3);
+INSERT INTO movie_genre (movie_id, genre_id) VALUES (3,4);
+INSERT INTO movie_genre (movie_id, genre_id) VALUES (4,4);
+INSERT INTO movie_genre (movie_id, genre_id) VALUES (4,6);
+
 
 
