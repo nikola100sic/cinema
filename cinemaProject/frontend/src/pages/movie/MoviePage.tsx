72c2433379a8df72d9c299b2d2aa04fb67ee95b8
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2024-11-03 15:33:47 +0100

    Added logic for delete movies

--- cinemaProject/frontend/src/pages/movie/MoviePage.tsx
+++ cinemaProject/frontend/src/pages/movie/MoviePage.tsx
@@ -8,10 +8,14 @@ import {
   AddButtonContainer,
   MovieCardContainer,
 } from './MoviePage.styled';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import Button from '../../components/shared/button/Button';
 
 const MoviePage = () => {
   const [movies, setMovies] = useState<Movie[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
+  const navigate = useNavigate();
 
   const getMovies = () => {
     movieServiceAxios
@@ -30,7 +34,49 @@ const MoviePage = () => {
     getMovies();
   }, []);
 
-  const goToAdd = () => {};
+  const goToAdd = () => {
+    navigate('/movies/add');
+  };
+
+  const goToEdit = (movieId: any) => {
+    navigate('edit/' + movieId);
+  };
+
+  const handleDelete = (movieId: number) => {
+    toast(
+      <>
+        <div>Are you sure you want to delete this genre?</div>
+        <Button
+          text="Yes"
+          variant="filled"
+          onClick={() => confirmDelete(movieId)}
+        />
+        <Button text="No" variant="outlined" onClick={() => toast.dismiss()} />
+      </>,
+      {
+        autoClose: false,
+        closeButton: false,
+      },
+    );
+  };
+
+  const confirmDelete = (id: number) => {
+    toast.dismiss();
+
+    movieServiceAxios
+      .deleteMovie(id)
+      .then((res) => {
+        console.log(res);
+        toast.success('Successfully deleted');
+        setMovies((prevMovies) =>
+          prevMovies.filter((movie) => movie.id !== id),
+        );
+      })
+      .catch((error) => {
+        console.log(error);
+        toast.error('Error');
+      });
+  };
   return (
     <>
       <AddButtonContainer>
@@ -48,6 +94,8 @@ const MoviePage = () => {
               duration={movie.duration}
               image={movie.image}
               genres={movie.genres}
+              onDelete={handleDelete}
+              onEdit={goToEdit}
             />
           ))}
         </MovieCardContainer>
