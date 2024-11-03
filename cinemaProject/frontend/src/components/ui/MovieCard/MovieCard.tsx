b7435a7499eab1c934b46352adcb5845fcb904af
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2024-11-03 13:33:04 +0100

    Updated MovieCard.tsx

--- cinemaProject/frontend/src/components/ui/MovieCard/MovieCard.tsx
+++ cinemaProject/frontend/src/components/ui/MovieCard/MovieCard.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Genre } from '../../../types/Genre';
+import { Label, MovieCardStyled, MovieInfo } from './MovieCard.styled';
 interface MovieCardProps {
   id: number;
   name: string;
   duration: number;
   image: string;
+  genres?: Genre[];
   onEdit?: (id: number) => void;
   onDelete?: (id: number) => void;
 }
@@ -13,10 +16,24 @@ const MovieCard = ({
   name,
   duration,
   image,
+  genres,
   onDelete,
   onEdit,
 }: MovieCardProps) => {
-  return <div></div>;
+  return (
+    <MovieCardStyled>
+      <MovieInfo>
+        <Label> Movie name:</Label>
+        {name}
+        <Label>Duration:</Label>
+        {duration}
+        <Label>Image:</Label>
+        {image}
+        <Label>Genres:</Label>
+        {genres?.map((genre, index) => <li key={index}>{genre.name}</li>)}
+      </MovieInfo>
+    </MovieCardStyled>
+  );
 };
 
 export default MovieCard;
