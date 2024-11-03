57563a50a5565cbbe3cb19f701c975d18e6cb21c
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2024-11-03 15:10:45 +0100

    Updated MovieCard.styled.ts

--- cinemaProject/frontend/src/components/ui/MovieCard/MovieCard.styled.ts
+++ cinemaProject/frontend/src/components/ui/MovieCard/MovieCard.styled.ts
@@ -7,8 +7,8 @@ export const MovieCardStyled = styled.div`
   border-radius: 18px;
   flex-direction: column;
   align-items: center;
+  flex: 0 1 calc(20% - 25px);
 `;
-
 export const MovieInfo = styled.div`
   display: flex;
   flex-direction: column;
@@ -19,4 +19,11 @@ export const MovieInfo = styled.div`
 export const Label = styled.span`
   font-weight: bold;
   font-size: 16px;
+`;
+
+export const ButtonContainer = styled.div`
+  display: flex;
+  gap: 58px;
+  justify-content: center;
+  margin-top: 20px;
 `;
