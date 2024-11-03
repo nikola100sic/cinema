65782154615aa8f21bb737a91b65a6e5b4800eab
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2024-11-03 13:34:55 +0100

    Updated MoviePage.tsx

--- cinemaProject/frontend/src/pages/movie/MoviePage.tsx
+++ cinemaProject/frontend/src/pages/movie/MoviePage.tsx
@@ -1,10 +1,58 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import MovieCard from '../../components/ui/MovieCard/MovieCard';
+import { Movie } from '../../types/Movie';
+import movieServiceAxios from '../../components/api/movie.service.axios';
+import Loader from '../../components/shared/loader/Loader';
+import {
+  AddButton,
+  AddButtonContainer,
+  MovieCardContainer,
+} from './MoviePage.styled';
 
 const MoviePage = () => {
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  const getMovies = () => {
+    movieServiceAxios
+      .getMovies()
+      .then((res) => {
+        setMovies(res.data);
+        console.log(res.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  const goToAdd = () => {};
   return (
-    <div>
-      <h2>Movie</h2>
-    </div>
+    <>
+      <AddButtonContainer>
+        <AddButton onClick={() => goToAdd()}>Add new movie</AddButton>
+      </AddButtonContainer>
+      {loading ? (
+        <Loader loading={loading} />
+      ) : (
+        <MovieCardContainer>
+          {movies.map((movie) => (
+            <MovieCard
+              key={movie.id}
+              name={movie.name}
+              id={movie.id}
+              duration={movie.duration}
+              image={movie.image}
+              genres={movie.genres}
+            />
+          ))}
+        </MovieCardContainer>
+      )}
+    </>
   );
 };
 
