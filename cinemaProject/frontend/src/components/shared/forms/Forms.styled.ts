54f3da0298e6b599148d958c5a832e597f25fd65
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2024-11-04 22:33:28 +0100

    Updated Form.styled.ts

--- cinemaProject/frontend/src/components/shared/forms/Forms.styled.ts
+++ cinemaProject/frontend/src/components/shared/forms/Forms.styled.ts
@@ -1,12 +1,19 @@
 import styled from 'styled-components';
 
-export const AddPageStyled = styled.div`
+export const AddGenrePageStyled = styled.div`
+  display: flex;
+  flex-direction: column;
   background: #3498db80;
   width: 400px;
   margin: 0 auto;
   padding: 30px;
   border-radius: 2px;
   box-shadow: -7px 13px 8px 0px rgba(0, 0, 0, 0.1);
+`;
+export const AddMoviePageStyled = styled(AddGenrePageStyled)`
+  flex-direction: row;
+  width: 50%;
+  background-color: #109bec;
 `;
 
 export const EditPageStyled = styled.div`
@@ -20,13 +27,17 @@ export const EditPageStyled = styled.div`
   margin-top: 100px;
 `;
 
-export const FormTitle = styled.h3`
+export const GenreFormTitle = styled.h3`
   text-align: center;
   color: #113775;
 `;
 
-export const FormInput = styled.input`
-  width: 100%;
+export const MovieFormTitle = styled(GenreFormTitle)`
+  text-align: left;
+`;
+
+export const GenreFormInput = styled.input`
+  width: 90%;
   padding: 10px;
   margin-bottom: 20px;
   border: 1px solid #ccc;
@@ -38,16 +49,21 @@ export const FormInput = styled.input`
   }
 `;
 
+export const MovieFormInput = styled(GenreFormInput)`
+  width: 80%;
+`;
+
 export const FormLabel = styled.label`
   display: block;
   margin: 10px 0 5px;
-  color: #555;
+  color: #ffffff;
+  font-size: large;
 `;
 
 export const CheckBoxGroup = styled.div`
   display: flex;
   flex-wrap: wrap;
-  gap: 7px;
+  gap: 3px;
 `;
 
 export const CheckBoxItem = styled.div`
@@ -60,4 +76,21 @@ export const CheckboxInput = styled.input`
   margin-right: 10px;
   cursor: pointer;
   transform: scale(1.4);
+`;
+
+export const MoviePicture = styled.div`
+  display: flex;
+  justify-content: center;
+  overflow: hidden;
+  height: 550px;
+  img {
+    width: auto;
+    max-width: 100%;
+    max-height: 100%;
+    object-fit: contain;
+  }
+`;
+export const MovieInfoAddPage = styled.div`
+  display: flex;
+  flex-direction: column;
 `;
