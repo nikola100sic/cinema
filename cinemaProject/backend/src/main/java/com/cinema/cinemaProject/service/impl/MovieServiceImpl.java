76da9be806d61c7944588c6069c6c242f0890760
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2024-11-07 20:44:37 +0100

    Edited MovieServiceImpl

--- cinemaProject/backend/src/main/java/com/cinema/cinemaProject/service/impl/MovieServiceImpl.java
+++ cinemaProject/backend/src/main/java/com/cinema/cinemaProject/service/impl/MovieServiceImpl.java
@@ -31,7 +31,7 @@ public class MovieServiceImpl implements MovieService {
 
     @Override
     public Page<Movie> findAll(String name, int durationFrom, int durationTo, int page) {
-        Page<Movie>movies = movieRepository.search(name,durationFrom, durationTo, PageRequest.of(page,10));
+        Page<Movie>movies = movieRepository.search(name,durationFrom, durationTo, PageRequest.of(page,5));
         if(movies.isEmpty()){
             throw  new MoviesListEmpty();
         }
