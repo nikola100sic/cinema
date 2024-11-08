8c96f1921c8b41896ba35d9ae9cdb60da3da66fb
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2024-11-08 22:41:15 +0100

    Added page method for pagination

--- cinemaProject/backend/src/main/java/com/cinema/cinemaProject/repository/GenreRepository.java
+++ cinemaProject/backend/src/main/java/com/cinema/cinemaProject/repository/GenreRepository.java
@@ -1,14 +1,17 @@
 package com.cinema.cinemaProject.repository;
 
 import com.cinema.cinemaProject.model.Genre;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface GenreRepository extends JpaRepository<Genre, Long> {
 
     Genre findOneById(Long id);
+
+    Page<Genre> findAll(Pageable pageable);
+
 
 }
