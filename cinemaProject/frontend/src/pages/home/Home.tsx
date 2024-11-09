84187328b88498c66905ec2ec7306375e3d4e4ca
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2024-11-09 13:33:20 +0100

    Created home page

---
+++ cinemaProject/frontend/src/pages/home/Home.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { StyledMarqueeText } from './Home.styled';
+
+const Screenings = () => {
+  return (
+    <>
+      <StyledMarqueeText>
+        <p>
+          Welcome to the world of movies! Don’t miss the latest screenings and
+          special offers.
+        </p>
+      </StyledMarqueeText>
+    </>
+  );
+};
+
+export default Screenings;
