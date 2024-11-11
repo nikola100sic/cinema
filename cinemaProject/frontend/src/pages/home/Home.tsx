245bbfe5680fd64e3c76ff325a489c1941745dfd
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2024-11-11 18:24:48 +0100

    Edited Home page

--- cinemaProject/frontend/src/pages/home/Home.tsx
+++ cinemaProject/frontend/src/pages/home/Home.tsx
@@ -1,16 +1,41 @@
-import React from 'react';
-import { StyledMarqueeText } from './Home.styled';
+import React, { useEffect, useState } from 'react';
+import { StyledHome, StyledMarqueeText, StyledScreenings } from './Home.styled';
+import DateButtonSelector from '../../components/ui/DataSelector/DateButtonSelector';
+import Button from '../../components/shared/button/Button';
 
 const Home = () => {
+  const [selectedDate, setSelectedDate] = useState('');
+  const [showMarquee, setShowMarquee] = useState(true);
+
+  const handleDateSelect = (date: string) => {
+    setSelectedDate(date);
+    console.log('Selected date', date);
+  };
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowMarquee(false);
+    }, 20000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <>
-      <StyledMarqueeText>
-        <p>
-          Welcome to the world of movies! Don't miss out! New at our cinema:
-          Joker movie premiere. Tickets starting at just 199.99 RSD.
-        </p>
-      </StyledMarqueeText>
+      {showMarquee && (
+        <StyledMarqueeText>
+          <p>
+            Welcome to the world of movies! Don't miss out! New at our cinema:
+            <b>Joker movie premiere.</b> Tickets starting at just{' '}
+            <i>199.99 RSD </i>.
+          </p>
+        </StyledMarqueeText>
+      )}
+      <StyledHome>
+        <DateButtonSelector onDateSelect={handleDateSelect} />
+        <StyledScreenings />
+      </StyledHome>
     </>
   );
 };
+
 export default Home;
