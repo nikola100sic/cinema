081b5775f04ebdaa549eee005742216aec923c23
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2024-11-11 18:25:05 +0100

    Edited home style

--- cinemaProject/frontend/src/pages/home/Home.styled.ts
+++ cinemaProject/frontend/src/pages/home/Home.styled.ts
@@ -1,7 +1,7 @@
 import styled, { keyframes } from 'styled-components';
 
 export const marquee = keyframes`
-  00% { transform: translateX(140%); }
+  00% { transform: translateX(175%); }
   100% { transform: translateX(-100%); }
 `;
 
@@ -11,12 +11,21 @@ export const StyledMarqueeText = styled.div`
   background-color: #f1f1f14f;
   overflow: hidden;
   white-space: nowrap;
+  margin-top: -20px;
 
   p {
     display: inline-block;
-    font-size: 14px;
+    font-size: 15px;
     animation: ${marquee} 20s linear infinite;
-    color: #0074f7;
-    font-family: monospace;
+    color: #e74c3cfa;
+    font-family: sans-serif;
   }
+`;
+
+export const StyledScreenings = styled.div`
+  display: flex;
+`;
+
+export const StyledHome = styled.div`
+  display: flex;
 `;
