2345dcaa7b43af4880927255f98b70230fbc036d
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2024-11-11 18:21:22 +0100

    Created Hall entity

---
+++ cinemaProject/backend/src/main/java/com/cinema/cinemaProject/model/Hall.java
@@ -0,0 +1,21 @@
+package com.cinema.cinemaProject.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Set;
+@Entity
+@Data
+@Table(name = "halls")
+public class Hall {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private int capacity;
+
+    @OneToMany(mappedBy = "hall", cascade = CascadeType.ALL)
+    private Set<Seat>seats;
+}
