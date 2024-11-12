4b1546edbae1bac46f6d1c6e21a28be088554a5b
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2024-11-12 13:01:28 +0100

    Updated screening entity

--- cinemaProject/backend/src/main/java/com/cinema/cinemaProject/model/Screening.java
+++ cinemaProject/backend/src/main/java/com/cinema/cinemaProject/model/Screening.java
@@ -3,7 +3,10 @@ package com.cinema.cinemaProject.model;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
+
 @Entity
 @Data
 public class Screening {
@@ -11,7 +14,10 @@ public class Screening {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private LocalDateTime dateTime;
+
+    private LocalDate screening_date;
+
+    private LocalTime screening_time;
 
     @ManyToOne
     private Movie movie;
