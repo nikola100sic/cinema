73b8090ee9c87c2de12ae19ab9380c14b6087c2d
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2024-11-12 12:56:41 +0100

    Created screening service

---
+++ cinemaProject/backend/src/main/java/com/cinema/cinemaProject/service/ScreeningService.java
@@ -0,0 +1,13 @@
+package com.cinema.cinemaProject.service;
+
+import com.cinema.cinemaProject.model.Screening;
+
+import java.util.List;
+
+public interface ScreeningService {
+
+    List<Screening> getAllScreenings();
+
+    Screening createScreening(Screening screening);
+
+}
