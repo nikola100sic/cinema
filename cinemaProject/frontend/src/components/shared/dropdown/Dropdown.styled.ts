74dc7475b5271df2a51821564a57766596dcecd6
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2024-11-14 23:23:06 +0100

    Created styles for dropdown

---
+++ cinemaProject/frontend/src/components/shared/dropdown/Dropdown.styled.ts
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+export const DropdownStyle = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-direction: row;
+  padding: 5px;
+  margin: 10px;
+`;
+
+export const Select = styled.select`
+  background-color: gainsboro;
+  font-style: italic;
+  font-size: 13px;
+  color: black;
+  padding: 8px;
+  border: 1px;
+  border-radius: 5px;
+  cursor: pointer;
+  padding: 12px;
+`;
