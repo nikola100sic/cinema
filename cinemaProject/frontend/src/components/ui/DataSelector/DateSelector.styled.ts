567362b2470bfd4eda70ea5d29761edab71f27d1
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2024-11-15 11:30:55 +0100

    Updated date selector

--- cinemaProject/frontend/src/components/ui/DataSelector/DateSelector.styled.ts
+++ cinemaProject/frontend/src/components/ui/DataSelector/DateSelector.styled.ts
@@ -3,8 +3,7 @@ import styled from 'styled-components';
 export const DateSelector = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: flex-start;
   margin-top: 7px;
   gap: 26px;
-  justify-content: center;
+  justify-content: space-around;
 `;
