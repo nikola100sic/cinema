9b5204b480bf2700f41a73791c14c3b0a43709f1
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2024-11-16 16:09:59 +0100

    Added delete method

--- cinemaProject/backend/src/main/java/com/cinema/cinemaProject/service/ScreeningService.java
+++ cinemaProject/backend/src/main/java/com/cinema/cinemaProject/service/ScreeningService.java
@@ -1,5 +1,6 @@
 package com.cinema.cinemaProject.service;
 
+import com.cinema.cinemaProject.model.Movie;
 import com.cinema.cinemaProject.model.Screening;
 
 import java.util.List;
@@ -9,5 +10,7 @@ public interface ScreeningService {
     List<Screening> getAllScreenings();
 
     Screening createScreening(Screening screening);
+
+    Screening delete (Long screeningId);
 
 }
