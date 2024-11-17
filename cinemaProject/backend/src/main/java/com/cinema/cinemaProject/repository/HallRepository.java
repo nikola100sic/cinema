c42d23b487c291dd5cc19fe2a1530ff055d952f2
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2024-11-17 19:21:02 +0100

    Created hall repository

---
+++ cinemaProject/backend/src/main/java/com/cinema/cinemaProject/repository/HallRepository.java
@@ -0,0 +1,7 @@
+package com.cinema.cinemaProject.repository;
+
+import com.cinema.cinemaProject.model.Hall;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HallRepository extends JpaRepository<Hall, Long> {
+}
