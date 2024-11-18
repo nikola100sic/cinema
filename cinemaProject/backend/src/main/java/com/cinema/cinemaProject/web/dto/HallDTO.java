b69f5f832cd2b0101be2b6e053ce6308d949a50d
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2024-11-19 00:12:14 +0100

    Created HallDTO

---
+++ cinemaProject/backend/src/main/java/com/cinema/cinemaProject/web/dto/HallDTO.java
@@ -0,0 +1,15 @@
+package com.cinema.cinemaProject.web.dto;
+
+import lombok.Data;
+
+@Data
+public class HallDTO {
+
+    private Long id;
+
+    private String name;
+
+    private int hallNumber;
+
+    private int capacity;
+}
