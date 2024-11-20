bd6ffed8b9e067625dba566fc1ffae54dbafcdf1
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2024-11-20 10:08:17 +0100

    Updated dropdown style

--- cinemaProject/frontend/src/components/shared/dropdown/Dropdown.styled.ts
+++ cinemaProject/frontend/src/components/shared/dropdown/Dropdown.styled.ts
@@ -1,20 +1,31 @@
 import styled from 'styled-components';
 
-export const DropdownStyle = styled.div`
+export const DropdownStyle = styled.div<{ type: string }>`
   display: flex;
-  justify-content: space-between;
+  justify-content: ${(props) =>
+    props.type === 'big' ? 'flex-start' : 'space-between'};
   flex-direction: row;
   margin-right: 27px;
+  width: ${(props) => (props.type === 'big' ? '100%' : 'auto')};
 `;
 
-export const Select = styled.select`
+export const Select = styled.select<{
+  $color: string;
+  $textColor: string;
+  $type: 'big' | 'small';
+}>`
   display: flex;
-  background-color: #1a72ac;
+  background-color: ${(props) => props.$color};
   font-style: italic;
-  color: white;
-  padding: 8px;
-  border: 1px;
-  border-radius: 12px;
+  color: ${(props) => props.$textColor};
+  padding: ${(props) => (props.$type === 'big' ? '11px' : '10px')};
+  font-size: ${(props) => (props.$type === 'big' ? '15px' : '14px')};
+  width: ${(props) => (props.$type === 'big' ? '97%' : 'auto')};
+  border-radius: 4px;
   cursor: pointer;
-  padding: 10px;
+  transition: background-color 0.3s ease;
+
+  &:hover {
+    background-color: ${(props) => props.$color};
+  }
 `;
