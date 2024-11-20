60d0545a7ba280fd41e9bc6d768b228d0c691ccd
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2024-11-20 10:47:21 +0100

    Added method to check for hall occupancy during screening creation

--- cinemaProject/backend/src/main/java/com/cinema/cinemaProject/repository/ScreeningRepository.java
+++ cinemaProject/backend/src/main/java/com/cinema/cinemaProject/repository/ScreeningRepository.java
@@ -6,11 +6,20 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
 
 public interface ScreeningRepository extends JpaRepository<Screening, Long> {
+
     @Query("SELECT s FROM Screening s WHERE s.screening_date = :date AND s.movie.id = :movieId")
     List<Screening> findByScreeningDateAndMovieId(@Param("date") LocalDate screeningDate, @Param("movieId") Long movieId);
+
+    @Query("SELECT s FROM Screening s WHERE s.hall.id = :hallId AND s.screening_date = :screeningDate")
+    List<Screening> findScreeningsByHallAndDate(
+            @Param("hallId") Long hallId,
+            @Param("screeningDate") LocalDate screeningDate);
+
+
 }
 
 
