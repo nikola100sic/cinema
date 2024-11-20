4088dc4908a431fd4672b1c2ff5b9337412a8a0a
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2024-11-20 10:47:52 +0100

    Updated method for adding new screening

--- cinemaProject/backend/src/main/java/com/cinema/cinemaProject/service/impl/ScreeningServiceImpl.java
+++ cinemaProject/backend/src/main/java/com/cinema/cinemaProject/service/impl/ScreeningServiceImpl.java
@@ -1,5 +1,6 @@
 package com.cinema.cinemaProject.service.impl;
 
+import com.cinema.cinemaProject.exception.screenings.HallOccupiedException;
 import com.cinema.cinemaProject.exception.screenings.ScreeningNotFoundException;
 import com.cinema.cinemaProject.exception.screenings.ScreeningsNotFoundException;
 import com.cinema.cinemaProject.model.Screening;
@@ -8,6 +9,7 @@ import com.cinema.cinemaProject.service.ScreeningService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalTime;
 import java.util.List;
 @Service
 @RequiredArgsConstructor
@@ -21,6 +23,18 @@ public class ScreeningServiceImpl implements ScreeningService {
 
     @Override
     public Screening createScreening(Screening screening) {
+        List<Screening> screenings = screeningRepository.findScreeningsByHallAndDate(
+                screening.getHall().getId(),
+                screening.getScreening_date()
+        );
+        LocalTime newEndTime = screening.getScreening_time().plusMinutes(screening.getMovie().getDuration());
+
+        for (Screening s : screenings) {
+            LocalTime existingEndTime = s.getScreening_time().plusMinutes(s.getMovie().getDuration());
+            if (screening.getScreening_time().isBefore(existingEndTime) && newEndTime.isAfter(s.getScreening_time())) {
+                throw new HallOccupiedException(screening.getScreening_time().toString());
+            }
+        }
         return screeningRepository.save(screening);
     }
 
