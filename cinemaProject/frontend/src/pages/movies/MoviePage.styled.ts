7858e18ca7495d491b0f2538491a0c00ede37bbb
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2024-11-22 10:19:33 +0100

    Updated movie page style

--- cinemaProject/frontend/src/pages/movies/MoviePage.styled.ts
+++ cinemaProject/frontend/src/pages/movies/MoviePage.styled.ts
@@ -1,22 +1,8 @@
 import styled from 'styled-components';
-import { StyledButton } from '../../components/shared/button/Button.styled';
 
 export const MovieCardContainer = styled.div`
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
   gap: 50px;
-`;
-
-export const AddButtonContainer = styled.div`
-  display: flex;
-  justify-content: flex-end;
-  margin-bottom: 12px;
-`;
-
-export const AddButton = styled(StyledButton)`
-  background-color: #00bcf7;
-  border: 1px solid #4a393952;
-  border-radius: 10px;
-  width: 129px;
 `;
