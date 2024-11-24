20e52ef32d6a199d15f066b95eb4f9e03d61ecc1
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2024-11-24 11:28:46 +0100

    Updated header

--- cinemaProject/frontend/src/components/shared/header/Header.tsx
+++ cinemaProject/frontend/src/components/shared/header/Header.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
   StyledHeader,
   StyledLinks,
   StyledLinksWrapper,
   StyledRegistrationLoginWrapper,
 } from './Header.styled';
+import Button from '../button/Button';
+import { FaUser, FaUserPlus } from 'react-icons/fa';
+import { jwtDecode } from 'jwt-decode';
 
 interface HeaderProps {
   brandName: string;
@@ -16,6 +19,29 @@ interface NavItem {
 }
 
 const Header = ({ brandName, navItems }: HeaderProps) => {
+  const token = localStorage.getItem('jwt');
+
+  const handleLogout = () => {
+    localStorage.removeItem('jwt');
+    window.location.replace('/home');
+  };
+
+  useEffect(() => {
+    if (token) {
+      try {
+        const decoded: any = jwtDecode(token);
+        const currentTime = Date.now() / 1000;
+        if (decoded.exp < currentTime) {
+          handleLogout();
+          window.location.reload();
+        }
+      } catch (error) {
+        console.error('Invalid token', error);
+        handleLogout();
+      }
+    }
+  }, [token]);
+
   return (
     <StyledHeader>
       <StyledLinks to={'/home'}>
@@ -29,8 +55,18 @@ const Header = ({ brandName, navItems }: HeaderProps) => {
         ))}
       </StyledLinksWrapper>
       <StyledRegistrationLoginWrapper>
-        <StyledLinks to={'/login'}>Login</StyledLinks>
-        <StyledLinks to={'/registration'}>Registration</StyledLinks>
+        {token ? (
+          <Button text="Logout" onClick={handleLogout} color="#00bcf7"></Button>
+        ) : (
+          <>
+            <StyledLinks to={'/login'}>
+              <FaUser style={{ marginRight: '5px' }} /> Login
+            </StyledLinks>
+            <StyledLinks to={'/registration'}>
+              <FaUserPlus style={{ marginRight: '5px' }} /> Registration
+            </StyledLinks>
+          </>
+        )}
       </StyledRegistrationLoginWrapper>
     </StyledHeader>
   );
