080e76e896572cd2f85f4563f2c9d4bb886724e7
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2024-11-24 11:29:01 +0100

    Updated layout

--- cinemaProject/frontend/src/components/shared/layout/Layout.tsx
+++ cinemaProject/frontend/src/components/shared/layout/Layout.tsx
@@ -2,23 +2,29 @@ import React, { ReactNode } from 'react';
 import { Content, StyledPageContainer } from './Layout.styled';
 import Header from '../header/Header';
 import Footer from '../footer/Footer';
+import useAuth from '../../../utils/authUtils';
 
 const NAVITEMS = [
   {
     title: 'Home',
     path: '/home',
+    requiresAuth: false,
   },
   {
     title: 'Genres',
     path: '/genres',
+    requiresAuth: true,
+    requiresAdmin: true,
   },
   {
     title: 'Movies',
     path: '/movies',
+    requiresAuth: true,
   },
   {
     title: 'Contact',
     path: '/contact',
+    requiresAuth: false,
   },
 ];
 
@@ -26,13 +32,23 @@ interface Props {
   children?: ReactNode;
 }
 
-const Layout = ({ children, ...props }: Props) => {
+const Layout = ({ children }: Props) => {
+  const token = localStorage.getItem('jwt');
+  const { isAdmin } = useAuth();
+
+  const filteredNavItems = NAVITEMS.filter((item) =>
+    item.requiresAuth ? (item.requiresAdmin ? isAdmin : token) : true,
+  );
   return (
     <StyledPageContainer>
-      <Header brandName="Cinema application" navItems={NAVITEMS}></Header>
+      <Header
+        brandName="Cinema application"
+        navItems={filteredNavItems}
+      ></Header>
       <Content>{children}</Content>
       <Footer text="© 2024 Cinema. All rights reserved. | Made by Nikola"></Footer>
     </StyledPageContainer>
   );
 };
+
 export default Layout;
