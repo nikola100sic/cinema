63174c63020aae5a7954ab4ed56b2c2fe3708ebc
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2024-11-25 17:16:29 +0100

    Created controller for users

--- cinemaProject/backend/src/main/java/com/cinema/cinemaProject/web/controller/AuthController.java
+++ cinemaProject/backend/src/main/java/com/cinema/cinemaProject/web/controller/AuthController.java
@@ -1,9 +1,10 @@
 package com.cinema.cinemaProject.web.controller;
 
-import com.cinema.cinemaProject.model.Genre;
+import com.cinema.cinemaProject.model.User;
 import com.cinema.cinemaProject.security.JwtUtills;
-import com.cinema.cinemaProject.service.GenreService;
+import com.cinema.cinemaProject.service.UserService;
 import com.cinema.cinemaProject.web.dto.LoginDTO;
+import com.cinema.cinemaProject.web.dto.RegistrationDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -11,10 +12,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,15 +23,33 @@ import java.util.List;
 public class AuthController {
 
     private final UserDetailsService userDetailsService;
+
     private final JwtUtills jwtUtills;
 
+    private final UserService userService;
+
+    private final PasswordEncoder passwordEncoder;
+
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAll() {
+        List<User> users = userService.findAll();
+        return new ResponseEntity<>(users, HttpStatus.OK);
+    }
+
     @PostMapping("/login")
-    public ResponseEntity<String> login(LoginDTO loginData) {
+    public ResponseEntity<String> login(@RequestBody LoginDTO loginData) {
         UserDetails user = userDetailsService.loadUserByUsername(loginData.getUsername());
-        if(user != null && user.getPassword().equals(loginData.getPassword())){
+        if (user != null && passwordEncoder.matches(loginData.getPassword(), user.getPassword())) {
             String token = jwtUtills.generateJwtToken(new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities()));
             return new ResponseEntity<>(token, HttpStatus.OK);
         }
         return new ResponseEntity<>("Invalid credentials.", HttpStatus.BAD_REQUEST);
+    }
+
+    @PostMapping("/registration")
+    public ResponseEntity<User> registration(@RequestBody RegistrationDTO registrationData) {
+        User user = userService.save(registrationData);
+        return new ResponseEntity<>(user, HttpStatus.OK);
     }
 }
