70faa55813189a522c0aff002cf5b216c0510471
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2024-11-26 19:37:57 +0100

    Updated LoginDTO

--- cinemaProject/backend/src/main/java/com/cinema/cinemaProject/web/dto/LoginDTO.java
+++ cinemaProject/backend/src/main/java/com/cinema/cinemaProject/web/dto/LoginDTO.java
@@ -3,11 +3,11 @@ package com.cinema.cinemaProject.web.dto;
 import lombok.Data;
 import org.springframework.stereotype.Component;
 
-@Component
 @Data
 public class LoginDTO {
 
     private String username;
 
     private String password;
+
 }
