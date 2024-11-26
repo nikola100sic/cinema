50bdcb35da24787f62e085d295ee9ce2c33c78b4
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2024-11-26 18:29:15 +0100

    Updated GlobalExceptionHandler

--- cinemaProject/backend/src/main/java/com/cinema/cinemaProject/exception/GlobalExceptionHandler.java
+++ cinemaProject/backend/src/main/java/com/cinema/cinemaProject/exception/GlobalExceptionHandler.java
@@ -7,6 +7,9 @@ import com.cinema.cinemaProject.exception.movies.MoviesListEmpty;
 import com.cinema.cinemaProject.exception.screenings.HallOccupiedException;
 import com.cinema.cinemaProject.exception.screenings.ScreeningNotFoundException;
 import com.cinema.cinemaProject.exception.screenings.ScreeningsNotFoundException;
+import com.cinema.cinemaProject.exception.users.InvalidCredentialsException;
+import com.cinema.cinemaProject.exception.users.NotVerifiedAccountException;
+import com.cinema.cinemaProject.exception.users.PasswordMismatchException;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,37 +20,52 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(GenreNotFoundException.class)
-    public ResponseEntity<String> handleEntityNotFoundException(GenreNotFoundException ex){
+    public ResponseEntity<String> handleEntityNotFoundException(GenreNotFoundException ex) {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(GenresListEmpty.class)
-    public ResponseEntity<String> handleEmptyListException(GenresListEmpty ex){
-        return new ResponseEntity<>(ex.getMessage(),HttpStatus.NOT_FOUND);
+    public ResponseEntity<String> handleEmptyListException(GenresListEmpty ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(MovieNotFoundException.class)
-    public ResponseEntity<String>handleEntityNotFoundException (MovieNotFoundException ex){
+    public ResponseEntity<String> handleEntityNotFoundException(MovieNotFoundException ex) {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(MoviesListEmpty.class)
-    public  ResponseEntity<String> handleEmptyListException(MoviesListEmpty ex){
+    public ResponseEntity<String> handleEmptyListException(MoviesListEmpty ex) {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(ScreeningsNotFoundException.class)
-    public  ResponseEntity<String> handleEmptyListException(ScreeningsNotFoundException ex){
+    public ResponseEntity<String> handleEmptyListException(ScreeningsNotFoundException ex) {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(ScreeningNotFoundException.class)
-    public ResponseEntity<String>handleEntityNotFoundException (ScreeningNotFoundException ex){
+    public ResponseEntity<String> handleEntityNotFoundException(ScreeningNotFoundException ex) {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
     }
 
+    @ExceptionHandler(PasswordMismatchException.class)
+    public ResponseEntity<String> handlePasswordMismatch(PasswordMismatchException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(InvalidCredentialsException.class)
+    public ResponseEntity<String> handleInvalidCredential(InvalidCredentialsException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(NotVerifiedAccountException.class)
+    public ResponseEntity<String> handleNotVerifiedAccount(NotVerifiedAccountException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.FORBIDDEN);
+    }
+    
     @ExceptionHandler(HallOccupiedException.class)
-    public ResponseEntity<String>handleEntityHallOccupiedException (HallOccupiedException ex){
+    public ResponseEntity<String> handleEntityHallOccupiedException(HallOccupiedException ex) {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
