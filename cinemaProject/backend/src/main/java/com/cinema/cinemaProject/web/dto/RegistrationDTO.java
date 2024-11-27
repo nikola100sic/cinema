757a70d19696539d945b938069070ed2cff3eadf
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2024-11-27 12:03:01 +0100

    Updated registration dto

--- cinemaProject/backend/src/main/java/com/cinema/cinemaProject/web/dto/RegistrationDTO.java
+++ cinemaProject/backend/src/main/java/com/cinema/cinemaProject/web/dto/RegistrationDTO.java
@@ -1,10 +1,6 @@
 package com.cinema.cinemaProject.web.dto;
 
-import com.cinema.cinemaProject.enumeration.UserRole;
-import jakarta.persistence.*;
 import lombok.Data;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Component;
 
 @Data
 public class RegistrationDTO extends UserDTO {
