00261f33a581f33944295712759bc4df0df78892
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2024-11-27 12:03:39 +0100

    Created styles for email verification page

---
+++ cinemaProject/frontend/src/pages/auth/EmailVerification.styled.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const VerificationPageStyled = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 150px;
+  p {
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    font-size: 20px;
+  }
+`;
