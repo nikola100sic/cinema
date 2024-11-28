11419c1a823e82470983ba5acb6a789358f9f920
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2024-11-28 13:55:37 +0100

    Updated jwt utils

--- cinemaProject/backend/src/main/java/com/cinema/cinemaProject/security/JwtUtills.java
+++ cinemaProject/backend/src/main/java/com/cinema/cinemaProject/security/JwtUtills.java
@@ -1,4 +1,5 @@
 package com.cinema.cinemaProject.security;
+
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
@@ -14,29 +15,31 @@ import java.util.Map;
 
 @Component
 public class JwtUtills {
-    @Value("4574398579348759834759843758fdgdfgfggdfgdfgdfgsecret123")
+
+    @Value("${jwt.secret}")
     private String jwtSecret;
 
-    @Value("360000")
-    private int jwtExpiration;
+    @Value("${jwt.expiration}")
+    private long jwtExpiration;
 
-    public String generateJwtToken(Authentication authentication){
+    public String generateJwtToken(Authentication authentication) {
         UserDetails userPricipal = (UserDetails) authentication.getPrincipal();
-        Map<String,String> claims = new HashMap<String,String>();
+        Map<String, String> claims = new HashMap<String, String>();
         claims.put("role", userPricipal.getAuthorities().stream().findFirst().get().getAuthority());
         return Jwts.builder()
                 .setClaims(claims)
                 .setSubject(userPricipal.getUsername())
                 .setIssuedAt(new Date())
-                .setExpiration(new Date(new Date().getTime()+jwtExpiration))
+                .setExpiration(new Date(new Date().getTime() + jwtExpiration))
                 .signWith(SignatureAlgorithm.HS256, jwtSecret)
                 .compact();
     }
-    public Key key(){
+
+    public Key key() {
         return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
     }
 
-    public  boolean validateJwtToken(String authtoken){
+    public boolean validateJwtToken(String authtoken) {
         try {
             Jwts.parserBuilder().setSigningKey(key()).build().parse(authtoken);
             return true;
@@ -45,7 +48,7 @@ public class JwtUtills {
         return false;
     }
 
-    public String getUsernameFromJwtToken(String authtoken){
+    public String getUsernameFromJwtToken(String authtoken) {
         return Jwts.parserBuilder().setSigningKey(key()).build().parseClaimsJws(authtoken).getBody().getSubject();
     }
 }
