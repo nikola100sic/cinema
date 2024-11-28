18767fabed513bfaf35023527f8c306e04698003
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2024-11-28 14:09:30 +0100

    Updated screening controller

--- cinemaProject/backend/src/main/java/com/cinema/cinemaProject/web/controller/ScreeningController.java
+++ cinemaProject/backend/src/main/java/com/cinema/cinemaProject/web/controller/ScreeningController.java
@@ -1,23 +1,25 @@
 package com.cinema.cinemaProject.web.controller;
 
+import com.cinema.cinemaProject.model.Genre;
 import com.cinema.cinemaProject.model.Movie;
 import com.cinema.cinemaProject.model.Screening;
 import com.cinema.cinemaProject.service.MovieService;
 import com.cinema.cinemaProject.service.ScreeningService;
 import com.cinema.cinemaProject.web.dto.MovieWithScreeningsDTO;
+import com.cinema.cinemaProject.web.dto.ScreeningDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/api/screenings",produces = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping(value = "/api/screenings", produces = MediaType.APPLICATION_JSON_VALUE)
 @RequiredArgsConstructor
 public class ScreeningController {
 
@@ -30,10 +32,12 @@ public class ScreeningController {
         List<Screening> allScreenings = screeningService.getAllScreenings();
         return new ResponseEntity<>(allScreenings, HttpStatus.OK);
     }
+
+    @PreAuthorize("permitAll()")
     @GetMapping
     public ResponseEntity<List<MovieWithScreeningsDTO>> getMoviesWithScreenings(
             @RequestParam(value = "date", required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date,
-            @RequestParam(value = "genreId", required = false)Long genreId) {
+            @RequestParam(value = "genreId", required = false) Long genreId) {
         if (date == null) {
             date = LocalDate.now();
         }
@@ -41,16 +45,23 @@ public class ScreeningController {
         return new ResponseEntity<>(screeningsFoDate, HttpStatus.OK);
     }
 
+//    @GetMapping("/{id}")
+//    public ResponseEntity<ScreeningDTO> getOne(@PathVariable Long id) {
+//        Screening screening = screeningService.findOne(id);
+//        return new ResponseEntity<>(genre, HttpStatus.OK);
+//    }
+
     @PostMapping
-    public ResponseEntity<Screening>createScreening (@RequestBody Screening screening){
+    public ResponseEntity<Screening> createScreening(@RequestBody Screening screening) {
         screeningService.createScreening(screening);
         return new ResponseEntity<>(screening, HttpStatus.OK);
 
     }
+
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void>delete(@PathVariable Long id){
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
         screeningService.delete(id);
-        return  new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
 }
