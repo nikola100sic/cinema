b63bcee28a72f616761b9aaa0291c01b6c32995c
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2024-11-28 14:03:49 +0100

    Updated hall entity

--- cinemaProject/backend/src/main/java/com/cinema/cinemaProject/model/Hall.java
+++ cinemaProject/backend/src/main/java/com/cinema/cinemaProject/model/Hall.java
@@ -1,13 +1,10 @@
 package com.cinema.cinemaProject.model;
 
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.Data;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 @Entity
 @Data
