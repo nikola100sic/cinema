c3debf3fd6ccec80c8248b26b2a032afacabe2b2
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2024-11-28 14:02:48 +0100

    Updated hall entity

--- cinemaProject/backend/src/main/java/com/cinema/cinemaProject/model/Hall.java
+++ cinemaProject/backend/src/main/java/com/cinema/cinemaProject/model/Hall.java
@@ -1,10 +1,14 @@
 package com.cinema.cinemaProject.model;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.Data;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+
 @Entity
 @Data
 @Table(name = "halls")
@@ -20,5 +24,6 @@ public class Hall {
     private int capacity;
 
     @OneToMany(mappedBy = "hall", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
-    private Set<Seat> seats = new HashSet<>();
+    private List<Seat> seats = new ArrayList<>() {
+    };
 }
