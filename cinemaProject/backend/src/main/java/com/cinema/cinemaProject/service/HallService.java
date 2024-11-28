8b759c46e354281aaade6eb831478852b4b669f7
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2024-11-28 14:07:20 +0100

    Removed unnecessary imports

--- cinemaProject/backend/src/main/java/com/cinema/cinemaProject/service/HallService.java
+++ cinemaProject/backend/src/main/java/com/cinema/cinemaProject/service/HallService.java
@@ -1,13 +1,11 @@
 package com.cinema.cinemaProject.service;
 
 import com.cinema.cinemaProject.model.Hall;
-import lombok.Data;
-import lombok.RequiredArgsConstructor;
 
 import java.util.List;
 
 public interface HallService {
 
-    List<Hall>findAll ();
+    List<Hall> findAll();
 
 }
