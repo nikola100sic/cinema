312c4c71a98900117f30c1785f6e34a89c228a53
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2024-11-30 15:15:18 +0100

    Updated exception for occupied hall

--- cinemaProject/backend/src/main/java/com/cinema/cinemaProject/exception/screenings/HallOccupiedException.java
+++ cinemaProject/backend/src/main/java/com/cinema/cinemaProject/exception/screenings/HallOccupiedException.java
@@ -1,7 +1,9 @@
 package com.cinema.cinemaProject.exception.screenings;
 
+import java.time.LocalTime;
+
 public class HallOccupiedException extends RuntimeException {
-    public HallOccupiedException(String time) {
-        super("The hall is occupied at the time: " + time);
+    public HallOccupiedException(LocalTime startTime, LocalTime endTime) {
+        super("The hall is occupied. Existing screening: " + startTime + " - " + endTime);
     }
 }
