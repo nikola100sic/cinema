d8535f334354fa5ad51990e45aa8af3afe18a813
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2024-12-06 22:53:18 +0100

    Updated account page

--- cinemaProject/frontend/src/pages/account/AccountPage.tsx
+++ cinemaProject/frontend/src/pages/account/AccountPage.tsx
@@ -1,11 +1,55 @@
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { User } from '../../types/User';
+import userServiceAxios from '../../components/api/user.service.axios';
+import { FaUserAlt } from 'react-icons/fa';
+import {
+  ImageStyle,
+  StyledCard,
+  UserContainer,
+  UserInformation,
+} from './AccountPage.styled';
+import { LeftSide, RightSide } from '../reservation/Reservation.styled';
 
 const AccountPage = () => {
   const { username } = useParams();
+  const [user, setUser] = useState<User>();
+
+  const getUser = async () => {
+    try {
+      const res = await userServiceAxios.getUser(String(username));
+      setUser(res.data);
+      console.log(res.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getUser();
+  }, []);
 
   return (
     <div>
-      <h4>Welcome to the account page, {username}!</h4>
+      <h4>
+        <UserContainer>
+          <LeftSide>
+            <ImageStyle>
+              <FaUserAlt style={{ height: 200, width: 180 }} />
+            </ImageStyle>
+            <UserInformation>
+              <p>Name: {user?.name}</p>
+              <p>Surname: {user?.surname}</p>
+              <p>Username: {user?.username}</p>
+              <p>eMail: {user?.email}</p>
+            </UserInformation>
+          </LeftSide>
+          <RightSide>
+            <StyledCard to="/my-reservations" image="/reservation.jpg" />
+            <StyledCard to="/my-favourites" image="/favourite.jpg" />
+          </RightSide>
+        </UserContainer>
+      </h4>
     </div>
   );
 };
