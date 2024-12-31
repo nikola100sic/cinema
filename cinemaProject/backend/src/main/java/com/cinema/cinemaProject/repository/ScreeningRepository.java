21e5e9002fb9179d48b7ddf9096981d9feecce85
Nikola Stošić <168572866+nikola100sic@users.noreply.github.com>
2024-12-31 12:21:34 +0100

    Updated screening repository class

--- cinemaProject/backend/src/main/java/com/cinema/cinemaProject/repository/ScreeningRepository.java
+++ cinemaProject/backend/src/main/java/com/cinema/cinemaProject/repository/ScreeningRepository.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDate;
-import java.time.LocalTime;
 import java.util.List;
 
 public interface ScreeningRepository extends JpaRepository<Screening, Long> {
